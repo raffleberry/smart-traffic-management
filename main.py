0ec99a1e2927f137ec8270a238395bfd7b46bbb9
raffleberry <raffleberry42@gmail.com>
2019-03-17 22:19:49 +0530

    fixed api bug

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from api import object_counting_api
 if tf.__version__ < '1.4.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
 
-input_video = 'ok.jpeg'
+input_video = 1
 
 # We use "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 detection_graph, category_index = backbone.set_model('ssdlite_mobilenet_v2_coco_2018_05_09')
@@ -21,4 +21,4 @@ is_color_recognition_enabled = 0
 
 #object_counting_api.targeted_object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled, targeted_objects, fps, width, height) # targeted objects counting
 
-object_counting_api.video(input_video, detection_graph, category_index, is_color_recognition_enabled, fps, width, height) # counting all the objects
+object_counting_api.object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled, fps, width, height) # counting all the objects
